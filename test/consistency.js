4668d8d3330e1402621f077369d1b18016ed8ae3
Meryn Stol <merynstol@gmail.com>
2013-04-10 15:22:02 +0200

    Test consistency on all package.json examples.

--- test/consistency.js
+++ test/consistency.js
@@ -3,16 +3,29 @@ var normalize = require("../lib/normalize")
 var path = require("path")
 var fs = require("fs")
 var _ = require("underscore")
+var async = require("async")
 
-tap.test("normalization consistency", function (t) {
-  var p = path.resolve(__dirname, "./fixtures/read-package-json.json")
-  fs.readFile (p, function (err, contents) {
-    if (err) throw err;
-    var data = JSON.parse(contents.toString())
-    normalize(data)
-    var clonedData = _.clone(data)
-    normalize(data)
-    t.deepEqual(data, clonedData, "Normalized normalized is normalized.")
-    t.end()
-  })
-})
+var data, clonedData
+
+tap.test("cosistent normalization", function(t) {
+  path.resolve(__dirname, "./fixtures/read-package-json.json")
+  fs.readdir (__dirname + "/fixtures", function (err, entries) {
+    verifyConsistency = function(entryName, next) {
+      filename = __dirname + "/fixtures/" + entryName
+      fs.readFile(filename, function(err, contents) {
+        if (err) return next(err)
+        data = JSON.parse(contents.toString())
+        normalize(data)
+        clonedData = _.clone(data)
+        normalize(data)
+        t.deepEqual(data, clonedData, 
+          "Normalization of " + entryName + "is consistent.")
+        next(null)
+      }) // fs.readFile
+    } // verifyConsistency
+    async.forEach(entries, verifyConsistency, function(err) {
+      if (err) throw err
+      t.end()
+    })
+  }) // fs.readdir
+}) // tap.test
