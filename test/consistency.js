c5b009d7122e448fd9e726ea5c1fe1bb1617920a
Meryn Stol <merynstol@gmail.com>
2013-04-12 12:47:13 +0200

    Add some ways to get warnings.

--- test/consistency.js
+++ test/consistency.js
@@ -6,19 +6,24 @@ var _ = require("underscore")
 var async = require("async")
 
 var data, clonedData
+var warn
 
 tap.test("consistent normalization", function(t) {
   path.resolve(__dirname, "./fixtures/read-package-json.json")
   fs.readdir (__dirname + "/fixtures", function (err, entries) {
+    // entries = ['coffee-script.json'] // uncomment to limit to a specific file
     verifyConsistency = function(entryName, next) {
+      warn = function(msg) { 
+        // t.equal("",msg) // uncomment to have some kind of logging of warnings
+      }
       filename = __dirname + "/fixtures/" + entryName
       fs.readFile(filename, function(err, contents) {
         if (err) return next(err)
         data = JSON.parse(contents.toString())
-        normalize(data)
+        normalize(data, warn)
         clonedData = _.clone(data)
-        normalize(data)
-        t.deepEqual(data, clonedData, 
+        normalize(data, warn)
+        t.deepEqual(clonedData, data,
           "Normalization of " + entryName + "is consistent.")
         next(null)
       }) // fs.readFile
